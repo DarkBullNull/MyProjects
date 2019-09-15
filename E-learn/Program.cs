bab005ca433dd9b425f99fbfecfb145b648f9c50
Maksim <43318993+skorpion5552012@users.noreply.github.com>
2019-09-15 20:18:39 +0300

    update

---
+++ E-learn/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace E_learn
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Newr:
+            Console.Write("Введите число: ");
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                string check = num.ToString();
+                char[] num1 = check.ToCharArray();
+                if (((check.Length == 6) & (num > 0)) | ((num < 0) & (check.Length == 7)))
+                {
+                    Console.WriteLine($"Первое число: " + num1[0] + "\nТретье число: " + num1[2] + "\nШестое число: " + num1[5]);
+                }
+                else
+                {
+                    Console.WriteLine("В числе должно быть 6 символов!");
+                    goto Newr;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Введи число!");
+                goto Newr;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LearnCSharp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Web;
+using System.IO;
+using System.Net;
+
+namespace LearnCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\Mythilogic\Desktop\note.txt";
+            File.Create(path).Dispose();
+        }
+    }
+}
