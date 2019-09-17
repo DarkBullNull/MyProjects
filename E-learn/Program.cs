5dd7c5b22d9a762a8a2ae2206df283c8aa0716de
Maksim <43318993+skorpion5552012@users.noreply.github.com>
2019-09-17 22:41:00 +0300

    update

--- E-learn/Program.cs
+++ E-learn/Program.cs
@@ -12,14 +12,18 @@ namespace E_learn
             {
                 int num = Convert.ToInt32(Console.ReadLine());
                 string check = num.ToString();
-                char[] num1 = check.ToCharArray();
+                string[] num1 = new string[6];
+                for (int i = 0, k = 0; i < 6; i++, k++)
+                {
+                    num1[i] = check.Substring(i,k);
+                }
                 if (((check.Length == 6) & (num > 0)) | ((num < 0) & (check.Length == 7)))
                 {
                     if (num < 0)
                     {
                         Console.WriteLine($"Первое число: " + num1[1] + "\nТретье число: " + num1[3] + "\nШестое число: " + num1[6]);
                     }
-                    else Console.WriteLine($"Первое число: " + num1[0] + "\nТретье число: " + num1[2] + "\nШестое число: " + num1[5]);
+                    else Console.WriteLine($"Первое число: {num1[0]}\nТретье число: {num1[2]}\nШестое число: {num1[5]}\nСумма: ");
                 }
                 else
                 {
