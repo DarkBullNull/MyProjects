795ab2b4ed5d5f0a58b9cd2faa7809f1ea746114
Maksim <43318993+skorpion5552012@users.noreply.github.com>
2019-09-18 19:13:20 +0300

    upd

--- E-learn/Program.cs
+++ E-learn/Program.cs
@@ -12,8 +12,15 @@ namespace E_learn
             {
                 int num = Convert.ToInt32(Console.ReadLine());
                 string check = num.ToString();
-                string[] num1 = new string[6];
-                for (int i = 0, k = 0; i < 6; i++, k++)
+                string[] num1 = new string[7];
+                if (check.Length == 6)
+                {
+                    for (int i = 0, k = 1; i < 6; i++)
+                    {
+                    num1[i] = check.Substring(i,k);
+                    }
+                }
+                else for (int i = 0, k = 1; i < 7; i++)
                 {
                     num1[i] = check.Substring(i,k);
                 }
@@ -21,9 +28,16 @@ namespace E_learn
                 {
                     if (num < 0)
                     {
-                        Console.WriteLine($"Первое число: " + num1[1] + "\nТретье число: " + num1[3] + "\nШестое число: " + num1[6]);
+                        Console.WriteLine("Первое число: " + num1[1] + "\nТретье число: " + num1[3] + "\nШестое число: " + num1[6] + "\nСумма: "
+                        + (Convert.ToInt32(num1[1]) + Convert.ToInt32(num1[3]) + Convert.ToInt32(num1[6]) + "\nРазность: " 
+                        + (Convert.ToInt32(num1[1]) - Convert.ToInt32(num1[3]) - Convert.ToInt32(num1[6])))
+                        );
                     }
-                    else Console.WriteLine($"Первое число: {num1[0]}\nТретье число: {num1[2]}\nШестое число: {num1[5]}\nСумма: ");
+                    else Console.WriteLine("Первое число: " + num1[0] + "\nТретье число: " + num1[2] + "\nШестое число: " + num1[5] + "\nСумма: " 
+                        + (Convert.ToInt32(num1[0])+Convert.ToInt32(num1[2])+Convert.ToInt32(num1[5]))
+                        + "\nРазность: "
+                        + (Convert.ToInt32(num1[0]) - Convert.ToInt32(num1[2]) - Convert.ToInt32(num1[5]))
+                        );
                 }
                 else
                 {
