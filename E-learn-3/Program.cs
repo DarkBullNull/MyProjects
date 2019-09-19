868c0ec821843987416d09f26828e5b8cf6930f5
Maksim <43318993+skorpion5552012@users.noreply.github.com>
2019-09-19 16:37:18 +0300

    updatename

---
+++ E-learn-3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace E_learning_3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Recheck:
+            try
+            {
+                double cat1, cat2, gip, ang1, ang2;
+                Console.Write("Введите длину первого катета: ");
+                cat1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите длину первого катета: ");
+                cat2 = Convert.ToInt32(Console.ReadLine());
+                if ((cat1 <= 0) | (cat2 <= 0))
+                {
+                    throw new Exception();
+                }
+                gip = (Math.Round(Math.Sqrt(cat1 * cat1 + cat2 * cat2), 2));
+                Console.WriteLine("Гипотенуза = " + gip);
+                ang1 = Math.Round(Math.Asin(cat2 / gip) * (180.0 / Math.PI), 0);
+                ang2 = Math.Round(Math.Asin(cat1 / gip) * (180.0 / Math.PI), 0);
+                Console.WriteLine("Угол альфа при катете, равном " + cat1 + " =  " + ang1);
+                Console.WriteLine("Угол бетта при катете, равном " + cat2 + " =  " + ang2);
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ищи ошибку");
+                goto Recheck;
+                throw;
+            }
+        }
+    }
+}
