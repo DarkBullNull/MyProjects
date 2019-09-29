f9e2df60fa8c02a7749b50f7d3328aaf61bcdf06
Maksim <43318993+skorpion5552012@users.noreply.github.com>
2019-09-29 21:38:17 +0300

    updated

--- TrinitySealGUIDE/Form1.cs
+++ TrinitySealGUIDE/Form1.cs
@@ -8,22 +8,40 @@ namespace TrinitySealGUIDE
 
     public partial class Form1 : Form
     {
-
+        public static int Prch { get; private set; }
         public Form1()
         {
             InitializeComponent();
-            AntiDump.Initialize();
-            AntiDebugging();
+            //AntiDump.Initialize();
+            //AntiDebugging();
+            
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        public void Form1_Load(object sender, EventArgs e)
         {
+            this.Opacity = 0D;
             SealCheck.HashChecks();
-           
+            Seal.GrabVariables("QhBvXsQr5qnwSMtwS76QdKzuK", "kgMKV1psE4yBD31fvxkbuMronNaZtS6haUJzEpvOtB77L", "Dark_Bull", "7580545910");
+            Seal.Initialize("1.1");
             if (SealCheck.isValidDLL)
             {
-                TrinitySeal.Seal.InitializeForm("kgMKV1psE4yBD31fvxkbuMronNaZtS6haUJzEpvOtB77L", "1.0", "QhBvXsQr5qnwSMtwS76QdKzuK", this, new MainForm(), SealColours.Purple);
+                if (ProgramVariables.Freemode)
+                {
+                    var mf = new MainForm();
+                    mf.Show();
+                    mf.Dispose();
+                }
+                else
+                {
+                    Prch = 1;
+                    Seal.InitializeForm(
+                    "kgMKV1psE4yBD31fvxkbuMronNaZtS6haUJzEpvOtB77L",
+                     "1.1",
+                    "QhBvXsQr5qnwSMtwS76QdKzuK",
+                    this, new MainForm(),
+                    SealColours.Purple);
+                }
             }
+            else Prch = 0;
         }
         public static async void AntiDebugging()
         {
@@ -32,6 +50,10 @@ namespace TrinitySealGUIDE
             Anti_DebugNET.AntiDebugTools.Scanner.ScanAndKill();
             goto reCheck;
         }
+        
+
+
 
     }
+
 }
--- TrinitySealGUIDE/MainForm.cs
+++ TrinitySealGUIDE/MainForm.cs
@@ -5,10 +5,19 @@ namespace TrinitySealGUIDE
 {
     public partial class MainForm : Form
     {
+
+        
         public MainForm()
         {
             InitializeComponent();
-
+            /*if (Form1.Prch == 1)
+            {
+                InitializeComponent();
+            }
+            else
+            {
+                Environment.Exit(1);
+            }*/
         }
 
         private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
