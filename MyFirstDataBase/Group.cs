0ebe4502312f7ad5d3b5ab9c19259be418a79bfc
Maksim <43318993+DarkBullNull@users.noreply.github.com>
2019-10-01 22:37:25 +0300

    started

---
+++ MyFirstDataBase/Group.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MyFirstDataBase
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Year { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
---
+++ MyFirstDataBase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyFirstDataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MyDbContext())
+            {
+                var group = new Group()
+                {
+                    Name = "Rammstein",
+                    Year = 1994
+                };
+
+                context.Groups.Add(group); //группа пока в кешированной БД
+                context.SaveChanges(); //все изменения из локального хранилища(кешированной БД) уйдут в основную БД
+
+                Console.WriteLine($"id: {group.Id}, name: {group.Name}, year: {group.Year}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
