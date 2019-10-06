96cc41fb989b27fcec64484956ce67640b77481b
Maksim <43318993+DarkBullNull@users.noreply.github.com>
2019-10-06 23:08:28 +0300

    update

--- WorkWithFiles/Form1.cs
+++ WorkWithFiles/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,20 @@ namespace WorkWithFiles
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(FBD.SelectedPath);
+            }
+            string oFile = File.ReadAllText(FBD.SelectedPath);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
         }
     }
 }
