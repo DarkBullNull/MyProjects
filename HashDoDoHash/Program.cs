b6d74cafb7f0bca6f36ab16b4b3470f95bb8d028
Maksim <43318993+DarkBullNull@users.noreply.github.com>
2019-10-21 23:50:02 +0300

    Project

---
+++ HashDoDoHash/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+
+namespace HashDoDoHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter path: ");
+            string path = Console.ReadLine();
+            path = path.Replace('"', ' ');
+            new Encoding(path);
+            new FileInformation(path);
+            Console.ReadKey();
+        }
+        
+        
+    }
+}
