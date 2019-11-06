6c39ed06923f3194277cc16fe4c84800610d7b95
Maksim <43318993+DarkBullNull@users.noreply.github.com>
2019-11-06 23:09:29 +0300

    fix strings

--- DiagTest/DiagTest/CommonStruct.cs
+++ DiagTest/DiagTest/CommonStruct.cs
@@ -2,32 +2,37 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DiagTest
 {
-    class CommonStruct
+    public class CommonStruct
     {
         public CommonStruct()
         {
             Assignment();
         }
-        static KeyValuePair<Image, string> Random0 { get; set; }
-        static KeyValuePair<Image, string> Random1 { get; set; }
-        static KeyValuePair<Image, string> Random2 { get; set; }
-        static KeyValuePair<Image, string> Random3 { get; set; }
-        static KeyValuePair<Image, string> Random4 { get; set; }
-        static public void Assignment()
+        public KeyValuePair<Image, string> Random0 { get; private set; }
+        public KeyValuePair<Image, string> Random1 { get; private set; }
+        public KeyValuePair<Image, string> Random2 { get; private set; }
+        public KeyValuePair<Image, string> Random3 { get; private set; }
+        public KeyValuePair<Image, string> Random4 { get; private set; }
+        void Assignment()
         {
             
             Random rnd0 = new Random();
             Random0 = Dictionary.diag.ElementAt(rnd0.Next(Dictionary.diag.Count));
+            Task.Delay(1000);
             Random rnd1 = new Random();
             Random1 = Dictionary.diag.ElementAt(rnd1.Next(Dictionary.diag.Count));
+            Task.Delay(1000);
             Random rnd2 = new Random();
             Random2 = Dictionary.diag.ElementAt(rnd2.Next(Dictionary.diag.Count));
+            Task.Delay(1000);
             Random rnd3 = new Random();
             Random3 = Dictionary.diag.ElementAt(rnd3.Next(Dictionary.diag.Count));
+            Task.Delay(1000);
             Random rnd4 = new Random();
             Random4 = Dictionary.diag.ElementAt(rnd4.Next(Dictionary.diag.Count));
         }
--- DiagTest/DiagTest/Form1.cs
+++ DiagTest/DiagTest/Form1.cs
@@ -9,20 +9,25 @@ namespace DiagTest
     public partial class Form1 : Form
     {
         CommonStruct cs = new CommonStruct();
-        int iterator = 0;
+        private int iterator = 0;
         public Form1()
         {
             InitializeComponent();
-            //pictureBox1.Image = ;
+            pictureBox1.Image = cs.Random0.Key;
         }
 
         
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //if (textBox1.Text == )
+            if (textBox1.Text == cs.Random0.Value)
             {
-                
+                pictureBox1.Image = cs.Random1.Key;
+                iterator++;
+            }
+            else
+            {
+                pictureBox1.Image = cs.Random1.Key;
             }
         }
     }
