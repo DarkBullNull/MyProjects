7747fd83ca8c0df965b93497f81190795be9dd8c
Maksim <43318993+DarkBullNull@users.noreply.github.com>
2019-11-06 21:47:36 +0300

    add Dictionary<>

---
+++ DiagrammsTest/DiagTest/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiagTest
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<Image, string> diag = new Dictionary<Image, string>
+            {
+                { ImagesQ.Img1, "7" },
+                { ImagesQ.Img2, "2" },
+                { ImagesQ.Img3, "3" },
+                { ImagesQ.Img4, "5" },
+                { ImagesQ.Img5, "2" },
+                { ImagesQ.Img6, "4" },
+                { ImagesQ.Img7, "8" },
+                { ImagesQ.Img8, "2" },
+                { ImagesQ.Img9, "7" },
+                { ImagesQ.Img10, "2" },
+                { ImagesQ.Img11, "12" },
+                { ImagesQ.Img12, "12" },
+                { ImagesQ.Img13, "5" },
+                { ImagesQ.Img14, "1" },
+                { ImagesQ.Img15, "5" },
+                { ImagesQ.Img16, "7" },
+                { ImagesQ.Img17, "3" },
+                { ImagesQ.Img18, "5" },
+                { ImagesQ.Img19, "6" },
+                { ImagesQ.Img20, "2" },
+            };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
