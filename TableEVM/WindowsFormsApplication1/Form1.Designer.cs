a4b020fbeb82c19cfa8375d076c38545011e4e23
Maksim <43318993+DarkBullNull@users.noreply.github.com>
2019-12-04 20:20:45 +0300

    ReleaseTableEVM

---
+++ TableEVM/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label_result_big = new System.Windows.Forms.Label();
+            this.label_result_small = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.Black;
+            this.dataGridView1.Location = new System.Drawing.Point(-2, -3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(558, 226);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label_result_big
+            // 
+            this.label_result_big.AutoSize = true;
+            this.label_result_big.Location = new System.Drawing.Point(12, 229);
+            this.label_result_big.Name = "label_result_big";
+            this.label_result_big.Size = new System.Drawing.Size(0, 13);
+            this.label_result_big.TabIndex = 1;
+            // 
+            // label_result_small
+            // 
+            this.label_result_small.AutoSize = true;
+            this.label_result_small.Location = new System.Drawing.Point(12, 260);
+            this.label_result_small.Name = "label_result_small";
+            this.label_result_small.Size = new System.Drawing.Size(0, 13);
+            this.label_result_small.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 289);
+            this.Controls.Add(this.label_result_small);
+            this.Controls.Add(this.label_result_big);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Table";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label_result_big;
+        private System.Windows.Forms.Label label_result_small;
+    }
+}
+
---
+++ TableEVM/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+            var rnd = new Random();
+            var column0 = new DataGridViewColumn();
+
+            
+
+
+
+
+
+            column0.HeaderText = ""; //текст в шапке
+            column0.Width = 100; //ширина колонки
+            column0.ReadOnly = true; //значение в этой колонке нельзя править
+            column0.Name = "key"; //текстовое имя колонки, его можно использовать вместо обращений по индексу
+            column0.Frozen = true; //флаг, что данная колонка всегда отображается на своем месте
+            column0.CellTemplate = new DataGridViewTextBoxCell(); //тип нашей колонки
+
+            var column1 = new DataGridViewColumn();
+            column1.HeaderText = "Зима";
+            column1.Name = "winter";
+            column1.CellTemplate = new DataGridViewTextBoxCell();
+
+            var column2 = new DataGridViewColumn();
+            column2.HeaderText = "Весна";
+            column2.Name = "spring";
+            column2.CellTemplate = new DataGridViewTextBoxCell();
+
+            var column3 = new DataGridViewColumn();
+            column3.HeaderText = "Лето";
+            column3.Name = "summer";
+            column3.CellTemplate = new DataGridViewTextBoxCell();
+
+
+            var column4 = new DataGridViewColumn();
+            column4.HeaderText = "Осень";
+            column4.Name = "autumn";
+            column4.CellTemplate = new DataGridViewTextBoxCell();
+
+            dataGridView1.Columns.Add(column0);
+            dataGridView1.Columns.Add(column1);
+            dataGridView1.Columns.Add(column2);
+            dataGridView1.Columns.Add(column3);
+            dataGridView1.Columns.Add(column4);
+
+            dataGridView1.AllowUserToAddRows = false;
+
+            for (int i = 1; i < 4; ++i)
+            {
+                //Добавляем строку, указывая значения колонок поочереди слева направо
+                dataGridView1.Rows.Add("Месяц " + i, rnd.Next(0, 1144) + " мм", rnd.Next(0, 1144) + " мм", rnd.Next(0, 1144) + " мм", rnd.Next(0, 1144) + " мм");
+            }
+
+
+            var sum_winter = 
+                  Convert.ToInt32(dataGridView1.Rows[0].Cells[1].Value.ToString().Replace(" мм", ""))
+                + Convert.ToInt32(dataGridView1.Rows[1].Cells[1].Value.ToString().Replace(" мм", ""))
+                + Convert.ToInt32(dataGridView1.Rows[2].Cells[1].Value.ToString().Replace(" мм", ""));
+            var sum_spring =
+                  Convert.ToInt32(dataGridView1.Rows[0].Cells[2].Value.ToString().Replace(" мм", ""))
+                + Convert.ToInt32(dataGridView1.Rows[1].Cells[2].Value.ToString().Replace(" мм", ""))
+                + Convert.ToInt32(dataGridView1.Rows[2].Cells[2].Value.ToString().Replace(" мм", ""));
+            var sum_summer =
+                  Convert.ToInt32(dataGridView1.Rows[0].Cells[3].Value.ToString().Replace(" мм", ""))
+                + Convert.ToInt32(dataGridView1.Rows[1].Cells[3].Value.ToString().Replace(" мм", ""))
+                + Convert.ToInt32(dataGridView1.Rows[2].Cells[3].Value.ToString().Replace(" мм", ""));
+            var sum_autumn =
+                  Convert.ToInt32(dataGridView1.Rows[0].Cells[4].Value.ToString().Replace(" мм", ""))
+                + Convert.ToInt32(dataGridView1.Rows[1].Cells[4].Value.ToString().Replace(" мм", ""))
+                + Convert.ToInt32(dataGridView1.Rows[2].Cells[4].Value.ToString().Replace(" мм", ""));
+            dataGridView1.Rows.Add("Всего осадков:", sum_winter + " мм", sum_spring + " мм", sum_summer + " мм", sum_autumn + " мм");
+            /*=======================================BIG=========================================*/
+            if (sum_autumn > sum_spring && sum_autumn > sum_winter && sum_autumn > sum_summer)
+            {
+                label_result_big.Text = "Больше всего осадков выпало осенью: " + sum_autumn + " мм";
+            }
+            else if (sum_winter > sum_spring && sum_winter > sum_summer && sum_winter > sum_autumn)
+            {
+                label_result_big.Text = "Больше всего осадков выпало зимой: " + sum_winter + " мм";
+            }
+            else if (sum_spring > sum_winter && sum_spring > sum_summer && sum_spring > sum_autumn)
+            {
+                label_result_big.Text = "Больше всего осадков выпало весной: " + sum_spring + " мм";
+            }
+            else if (sum_summer > sum_winter && sum_summer > sum_spring && sum_summer > sum_autumn)
+            {
+                label_result_big.Text = "Больше всего осадков выпало летом: " + sum_summer + " мм";
+            }
+            /*=======================================SMALL=========================================*/
+            if (sum_autumn < sum_spring && sum_autumn < sum_winter && sum_autumn < sum_summer)
+            {
+                label_result_small.Text = "Меньше всего осадков выпало осенью: " + sum_autumn + " мм";
+            }
+            else if (sum_winter < sum_spring && sum_winter < sum_summer && sum_winter < sum_autumn)
+            {
+                label_result_small.Text = "Меньше всего осадков выпало зимой: " + sum_winter + " мм";
+            }
+            else if (sum_spring < sum_winter && sum_spring < sum_summer && sum_spring < sum_autumn)
+            {
+                label_result_small.Text = "Меньше всего осадков выпало весной: " + sum_spring + " мм";
+            }
+            else if (sum_summer < sum_winter && sum_summer < sum_spring && sum_summer < sum_autumn)
+            {
+                label_result_small.Text = "Меньше всего осадков выпало летом: " + sum_summer + " мм";
+            }
+        }
+
+        
+    }
+}
